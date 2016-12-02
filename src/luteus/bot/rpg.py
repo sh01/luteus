dbda281d52d91a31d7547579ef9acfb1bb844315
Sebastian Hagen <sh_c_git@memespace.net>
2016-12-02 07:48:24 +0000

    bot/rpg: Generalize !roll to dicey arithmetic.

--- src/luteus/bot/rpg.py
+++ src/luteus/bot/rpg.py
@@ -16,7 +16,9 @@
 # along with luteus.  If not, see <http://www.gnu.org/licenses/>.
 
 from ..core.irc_ui import LuteusOP, OS, rch
+import operator
 import random
+import re
 
 class ModBase:
   def link_bot(self, bot):
@@ -27,6 +29,116 @@ def get_comment(ctx, skip):
   if (tail is None):
     return ''
   return '({})'.format(repr(tail)[1:])
+
+
+class SemanticError(Exception):
+  pass
+
+class SyntaxError(Exception):
+  pass
+
+class CloseParen(Exception):
+  pass
+
+class Operator:
+  def __init__(self, op, preval):
+    self.op = op
+    self.a0 = preval
+    self.a1 = None
+
+  def __int__(self):
+    return self.op(int(self.a0), int(self.a1))
+
+
+def roll_dice(count, sides):
+  """<count>d<sides> dice roll"""
+  count = int(count)
+  sides = int(sides)
+  if (count > 128):
+    raise SemanticError('Excessive dice count: {} > 128.'.format(count))
+  if (sides > 8192):
+    raise SemanticError('Excessive die sides: {} > 8192'.format(sides))
+
+  return sum(random.randint(1,sides) for _ in range(count))
+
+
+class RollTree:
+  operators = {
+    b'+': operator.add,
+    b'-': operator.sub,
+    b'*': operator.mul,
+    b'/': operator.floordiv,
+    b'd': roll_dice
+  }
+  RE_NUM = re.compile(b'[0-9]+')
+
+  def __init__(self, data):
+    self.data = memoryview(data)
+    self.idx = 0
+
+  def getc(self):
+    return self.data[self.idx:self.idx+1]
+
+  def eval(self):
+    self.idx = 0
+    p = self.parse()
+    return int(p)
+
+  def parse(self):
+    import time; time.sleep(1)
+    t = None
+    def push(val):
+      nonlocal t
+      if (t is None):
+        t = val
+        return
+      try:
+        a1 = t.a1
+      except Exception:
+        pass
+      else:
+        if (a1 is None):
+          t.a1 = val
+          return
+      raise SyntaxError('Value collision')
+    
+    while (self.idx < len(self.data)):
+      c = self.getc()
+      self.idx += 1
+      # Whitespace
+      if (c == b' '):
+        continue
+      # Parens
+      if (c == b'('):
+        try:
+          self.parse()
+        except CloseParen as exc:
+          push(exc.val)
+          continue
+        else:
+          raise SyntaxError('Unmatched "("')
+      if (c == b')'):
+        cp = CloseParen()
+        cp.val = t
+        raise cp
+      f = self.operators.get(c)
+
+      # Operators
+      if not (f is None):
+        if (t is None):
+          raise SyntaxError('Missing prefix value for infix operator {!a}'.format(bytes(c)))
+        t = Operator(f, t)
+        continue
+
+      # Numbers
+      m = self.RE_NUM.match(self.data, pos=self.idx-1)
+      if not (m is None):
+        self.idx = m.end()
+        push(int(self.data[m.start():self.idx]))
+        continue
+
+      raise SyntaxError('Unrecognized character {!a}'.format(bytes(c)))
+    return t
 
 
 class RPGMod(ModBase):
@@ -85,15 +197,12 @@ class RPGMod(ModBase):
 
   @rch("ROLL", "Generic die roll")
   def _roll_die(self, ctx, *args):
-    lim = 128
-    outs = []
-    for arg in args:
-      (c, s) = arg.split(b'd')
-      c = int(c)
-      s = int(s)
-      if (c > lim):
-        ctx.output('{} is above my dice-count limit of {}. I refuse.'.format(c, lim))
-        return
-      out = sum((random.randint(1,s) for i in range(c)))
-      outs.append('{}d{}={}'.format(c,s,out))
-    ctx.output('ROLL: ' + ' '.join(outs))
+    text = ctx.get_cmd_tail(1)
+    t = RollTree(text)
+    try:
+      val = t.eval()
+    except Exception as exc:
+      ctx.output('{}: {}'.format(type(exc).__name__, exc))
+      return
+
+    ctx.output('ROLL: {}'.format(val))
